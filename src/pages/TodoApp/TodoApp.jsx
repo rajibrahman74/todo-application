dfeb4430604f1b719a40bd20226a0626df84c649
Rajibur Rahman <rajiburrahman74@gmail.com>
2023-06-23 01:38:47 +0600

    Error: try to resolve

--- src/pages/TodoApp/TodoApp.jsx
+++ src/pages/TodoApp/TodoApp.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
@@ -5,26 +7,29 @@ import SearchTodo from "./SearchTodo";
 import Head from "next/head";
 
 const TodoApp = () => {
-  const [todos, setTodos] = useState([]);
+  const [originalTodos, setOriginalTodos] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
   const addTodo = (title) => {
     const newTodo = {
       id: Date.now(),
       title,
     };
-    setTodos([...todos, newTodo]);
+    setOriginalTodos([...originalTodos, newTodo]);
+    setFilteredTodos([...originalTodos, newTodo]);
   };
 
   const deleteTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
+    const updatedTodos = originalTodos.filter((todo) => todo.id !== id);
+    setOriginalTodos(updatedTodos);
+    setFilteredTodos(updatedTodos);
   };
 
   const searchTodo = (query) => {
-    const filteredTodos = todos.filter((todo) =>
+    const filteredTodos = originalTodos.filter((todo) =>
       todo.title.toLowerCase().includes(query.toLowerCase())
     );
-    setTodos(filteredTodos);
+    setFilteredTodos(filteredTodos);
   };
 
   return (
@@ -35,7 +40,7 @@ const TodoApp = () => {
       <h1 className="text-center text-3xl font-bold mb-4">Todo App</h1>
       <TodoForm onSubmit={addTodo} />
       <SearchTodo onSearch={searchTodo} />
-      <TodoList todos={todos} onDelete={deleteTodo} />
+      <TodoList todos={filteredTodos} onDelete={deleteTodo} />
     </div>
   );
 };
